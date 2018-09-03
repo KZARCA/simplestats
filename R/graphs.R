bcca0599a26e0340bfe2b8eda895d164723d9c29
KZARCA <kevin.zarca@gmail.com>
2018-09-03 17:47:53 +0200

    premier merge

--- R/graphs.R
+++ R/graphs.R
@@ -13,7 +13,7 @@ print_plot_desc <- function(tab, vardep = NULL, varindep = NULL, type = "linear"
   if (type == "survival"){
     formule <- sprintf("Surv(.time, %s) ~ 1", vardep)
     r <- survfit(as.formula(formule), data = tab)
-    ggsurv(r, ylab=label(tab[[vardep]]), cens.shape = "|", back.white = TRUE)  + scale_y_continuous(labels = scales::percent)
+    ggkm(r, ylab=label(tab[[vardep]]), cens.shape = "|", back.white = TRUE)  + scale_y_continuous(labels = scales::percent)
   } else if (is.numeric(tab[[varindep]])){
     ggplot(remove_missing(tab, na.rm = TRUE, vars = varindep)) + aes_string(x = varindep) + geom_histogram() + theme_bw() + labs(y = "Number")
   } else {
@@ -56,7 +56,7 @@ print_plot_bivar <- function(tab, vardep, varindep, type = "linear"){
       formule <- sprintf("Surv(.time, %s) ~ %s", vardep, varindep)
       r <- survfit(as.formula(formule), data = tab)
       names(r$strata) <- gsub(varindep, label(tvarn), names(r$strata))
-      ggsurv(r, ylab=label(tab[[vardep]]), back.white = TRUE, cens.shape = "|") + scale_y_continuous(labels = scales::percent)
+      ggkm(r, ylab=label(tab[[vardep]]), back.white = TRUE, cens.shape = "|") + scale_y_continuous(labels = scales::percent)
     }
   }
 }
@@ -104,7 +104,7 @@ boxplot_bivar_bw <- function(tab, x, y) {
 #' @param x the independant variable
 #' @param y the dependant variable
 #'
-#' @return a ggplot boxplot
+#' @return a ggplot barplot
 #' @export
 #'
 #' @examples
@@ -141,4 +141,240 @@ barplot_bivar <- function(tab, x, y, graphPercent = NULL, showGraphNA = NULL){
 
   graph <- graph + theme_bw()  + scale_x_discrete(breaks = NULL)
   return(graph)
+}
+
+
+#' @export
+ggkm <- function(sfit,
+                   table = FALSE,
+                   xlabs = gettext("Time"),
+                   ylabs = gettext("Survival"),
+                   xlims = c(0, max(sfit$time)),
+                   ylims = c(0,1),
+                   ystratalabs = names(sfit$strata),
+                   ystrataname = "",
+                   main = "",
+                   pval = FALSE,
+                   marks = TRUE,
+                   shape = "|",
+                   legend = TRUE,
+                   ci = FALSE,
+                   subs = NULL,
+                   linecols="default",
+                   dashed= FALSE,
+                   ...) {
+
+  breaks = scales::pretty_breaks(4)(sfit$time)
+    #################################
+    # sorting the use of subsetting #
+    #################################
+    if(is.null(subs)){
+      if(length(levels(summary(sfit)$strata)) == 0) {
+        subs1 <- 1
+        subs2 <- 1:length(summary(sfit,censored=T)$time)
+        subs3 <- 1:length(summary(sfit,times = breaks,extend = TRUE)$time)
+      } else {
+        subs1 <- 1:length(levels(summary(sfit)$strata))
+        subs2 <- 1:length(summary(sfit,censored=T)$strata)
+        subs3 <- 1:length(summary(sfit,times = breaks,extend = TRUE)$strata)
+      }
+    } else{
+      for(i in 1:length(subs)){
+        if(i==1){
+          ssvar <- paste("(?=.*\\b=",subs[i],sep="")
+        }
+        if(i==length(subs)){
+          ssvar <- paste(ssvar,"\\b)(?=.*\\b=",subs[i],"\\b)",sep="")
+        }
+        if(!i %in% c(1, length(subs))){
+          ssvar <- paste(ssvar,"\\b)(?=.*\\b=",subs[i],sep="")
+        }
+        if(i==1 & i==length(subs)){
+          ssvar <- paste("(?=.*\\b=",subs[i],"\\b)",sep="")
+        }
+      }
+      subs1 <- which(regexpr(ssvar,levels(summary(sfit)$strata), perl=T)!=-1)
+      subs2 <- which(regexpr(ssvar,summary(sfit,censored=T)$strata, perl=T)!=-1)
+      subs3 <- which(regexpr(ssvar,summary(sfit,times = breaks,extend = TRUE)$strata, perl=T)!=-1)
+    }
+
+    if(!is.null(subs)) pval <- FALSE
+
+    ##################################
+    # data manipulation pre-plotting #
+    ##################################
+
+
+
+    if(length(levels(summary(sfit)$strata)) == 0) {
+      #[subs1]
+      if(is.null(ystratalabs)) ystratalabs <- as.character(sub("group=*","","All"))
+    } else {
+      #[subs1]
+      if(is.null(ystratalabs)) ystratalabs <- as.character(sub("group=*","",names(sfit$strata)))
+    }
+
+    if(is.null(ystrataname)) ystrataname <- "Strata"
+    m <- max(nchar(ystratalabs))
+
+    if(length(levels(summary(sfit)$strata)) == 0) {
+      Factor <- factor(rep("All",length(subs2)))
+    } else {
+      Factor <- factor(summary(sfit, censored = T)$strata[subs2])
+    }
+
+    #Data to be used in the survival plot
+    df <- data.frame(
+      time = sfit$time[subs2],
+      n.risk = sfit$n.risk[subs2],
+      n.event = sfit$n.event[subs2],
+      n.censor = sfit$n.censor[subs2],
+      surv = sfit$surv[subs2],
+      strata = Factor,
+      upper = sfit$upper[subs2],
+      lower = sfit$lower[subs2]
+    )
+
+    #Final changes to data for survival plot
+    levels(df$strata) <- ystratalabs
+    zeros <- data.frame(time = 0, surv = 1,
+                        strata = factor(ystratalabs, levels=levels(df$strata)),
+                        upper = 1, lower = 1)
+    df <- dplyr::bind_rows(zeros, df)
+    d <- length(levels(df$strata))
+
+    print(levels(df$strata))
+
+    ###################################
+    # specifying axis parameteres etc #
+    ###################################
+
+    if(dashed == TRUE){
+      linetype=c("solid", "dashed", "dotted", "dotdash", "longdash", "twodash", "1F", "F1", "4C88C488", "12345678")
+    } else {
+      linetype=c("solid", "solid", "solid", "solid", "solid", "solid", "solid", "solid", "solid", "solid", "solid")
+    }
+
+
+    p <- ggplot( df, aes(x=time, y=surv, colour=strata, linetype=strata)) +
+      ggtitle(main)
+
+    p <- p + theme_bw() +
+      theme(legend.position = "top") +
+      scale_x_continuous(xlabs, breaks = breaks, limits = xlims) +
+      scale_y_continuous(ylabs, limits = ylims, labels = scales::percent)
+
+    if (table == TRUE){
+      #Set up theme elements
+      p <- p +
+        theme(axis.title.x = element_text(vjust = 0.7),
+              axis.line = element_line(size =0.5, colour = "black"),
+              plot.margin = unit(c(0, 1, .5,ifelse(m < 10, 1.5, 2.5)),"lines"),
+              axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
+              axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black")
+              )
+    }
+
+
+
+
+
+    #Add 95% CI to plot
+
+    if(ci == TRUE & length(levels(summary(sfit)$strata)) == 0){
+      p <- p + geom_step(aes(y = upper), color = "black", lty = 2, size = 0.5) +
+        geom_step(aes(y = lower), color = "black", lty = 2, size = 0.5)
+    } else if (ci == TRUE){
+      p <- p +  geom_ribbon(data=df, aes(ymin = lower, ymax = upper), fill = "grey", alpha=0.25, colour=NA)
+    }
+
+    #Removes the legend:
+    if(legend == FALSE)
+      p <- p + theme(legend.position="none")
+
+    #Add lines to plot
+    p <- p + geom_step(size = 0.75) +
+      scale_linetype_manual(name = ystrataname, values=linetype)
+    if (linecols != "default") {
+      p <- p + scale_colour_brewer(name = ystrataname, palette=linecols)
+    } else {
+      p <- p + scale_colour_hue(name = ystrataname)
+    }
+    #Add censoring marks to the line:
+    if(marks == TRUE)
+      p <- p + geom_point(data = subset(df, n.censor >= 1), aes(x = time, y = surv, colour = strata), shape = shape) +
+      guides(colour = guide_legend(override.aes = list(shape = NA)))
+
+    ## Create a blank plot for place-holding
+    blank.pic <- ggplot(df, aes(time, surv)) +
+      geom_blank() + theme_bw() +
+      theme(axis.text.x = element_blank(),axis.text.y = element_blank(),
+            axis.title.x = element_blank(),axis.title.y = element_blank(),
+            axis.ticks = element_blank(),
+            panel.grid.major = element_blank(),panel.border = element_blank())
+
+    #####################
+    # p-value placement #
+    #####################a
+
+    if(length(levels(summary(sfit)$strata)) == 0) pval <- FALSE
+
+    if(pval == TRUE) {
+      sdiff <- survdiff(eval(sfit$call$formula), data = eval(sfit$call$data))
+      pvalue <- pchisq(sdiff$chisq,length(sdiff$n) - 1,lower.tail = FALSE)
+      pvaltxt <- ifelse(pvalue < 0.0001,"p < 0.0001",paste("p =", signif(pvalue, 3)))
+      # MOVE P-VALUE LEGEND HERE BELOW [set x and y]
+      p <- p + annotate("text",x = (as.integer(max(sfit$time)/5)), y = 0.1,label = pvaltxt)
+    }
+
+    ###################################################
+    # Create table graphic to include at-risk numbers #
+    ###################################################
+
+    if(length(levels(summary(sfit)$strata)) == 0) {
+      Factor <- factor(rep("All",length(subs3)))
+    } else {
+      Factor <- factor(summary(sfit,times = breaks,extend = TRUE)$strata[subs3])
+    }
+
+    if(table == TRUE) {
+      risk.data <- data.frame(
+        strata = Factor,
+        time = summary(sfit,times = breaks,extend = TRUE)$time[subs3],
+        n.risk = summary(sfit,times = breaks,extend = TRUE)$n.risk[subs3]
+      )
+
+      risk.data$strata <- factor(risk.data$strata, levels=rev(levels(risk.data$strata)))
+
+      data.table <- ggplot(risk.data,aes(x = time, y = strata, label = format(n.risk, nsmall = 0))) +
+        geom_text(size = 3.5) + theme_bw() +
+        scale_y_discrete(breaks = as.character(levels(risk.data$strata)),
+                         labels = rev(ystratalabs)) +
+        scale_x_continuous(gettext("Numbers at risk"), limits = xlims) +
+        theme(axis.title.x = element_text(size = 10, vjust = 1),
+              panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+              panel.border = element_blank(),axis.text.x = element_blank(),
+              axis.ticks = element_blank(),axis.text.y = element_text(face = "bold",hjust = 1))
+
+      data.table <- data.table +
+        theme(legend.position = "none") + xlab(NULL) + ylab(NULL)
+
+
+      # ADJUST POSITION OF TABLE FOR AT RISK
+      data.table <- data.table +
+        theme(plot.margin = unit(c(-1.5, 1, 0.1, ifelse(m < 10, 2.5, 3.5) - 0.15 * m), "lines"))
+    }
+
+
+    #######################
+    # Plotting the graphs #
+    #######################
+
+    if(table == TRUE){
+      grid.arrange(p, blank.pic, data.table, clip = FALSE, nrow = 3,
+                   ncol = 1, heights = unit(c(2, .1, .25),c("null", "null", "null")))
+    } else {
+      p
+    }
+
 }
